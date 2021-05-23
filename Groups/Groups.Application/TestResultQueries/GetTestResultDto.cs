9377c0ed7962c8e3e407f4c7f33d5fefb8efc71e
Michson07 Michson07 <mczajczej@gmail.com>
2021-05-24 00:12:27 +0200

    testresult

---
+++ Groups/Groups.Application/TestResultQueries/GetTestResultDto.cs
@@ -0,0 +1,12 @@
+﻿using Core.Application;
+using Groups.Application.TestResultQueries;
+using MediatR;
+
+namespace Groups.Application
+{
+    public class GetTestResultDto : IRequest<QueryResult<TestResultView>>
+    {
+        public string Email { get; set; } = string.Empty;
+        public string TestId { get; set; } = string.Empty;
+    }
+}
---
+++ Groups/Groups.Application/TestResultQueries/TestResultView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Groups.Application.TestResultQueries
+{
+    public class TestResultView
+    {
+        public string? State { get; set; }
+        public int ReceivedPoints { get; set; }
+        public string? InfoAboutPoints { get; set; }
+        public IEnumerable<QuestionView> Questions { get; set; } = new List<QuestionView>();
+    }
+
+    public class QuestionView
+    {
+        public string Question { get; set; } = string.Empty;
+        public int ReceivedPoints { get; set; } = 0;
+        public int PossiblePoints { get; set; } = 0;
+        public IEnumerable<string>? UserAnswers { get; set; }
+        public IEnumerable<string>? CorrectAnswers { get; set; }
+    }
+}
